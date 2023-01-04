bd739a8c729b4a47094ae143e8bf785c5b8461c0
Impartial_Raven <kolus7381@gmail.com>
2023-01-04 23:21:45 +0100

    new modifications

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      routes: {
+        'firstWelcomScreen': (context) => const WelcomeScreen(),
+        'secondWelcomeScreen': (context) => const SecondScreen()
+      },
+      home: const Scaffold(
         body: WelcomeScreen(),
       ),
     );
--- lib/screens/welcomescreen.dart
+++ lib/screens/welcomescreen.dart
@@ -15,6 +15,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.width,
       decoration: const BoxDecoration(
         // gradient: LinearGradient(
         //     stops: [0.2, 0.7],
@@ -90,7 +91,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                       )
                     ],
                   ),
-                  SizedBox(height: 90.0),
+                  const SizedBox(height: 90.0),
                   Align(
                     alignment: Alignment.bottomCenter,
                     child: Row(
@@ -104,10 +105,13 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                                   color: Colors.white, fontSize: 17.0),
                             )),
                         IconButton(
-                            onPressed: () {},
-                            icon: Icon(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                  context, 'secondWelcomeScreen');
+                            },
+                            icon: const Icon(
                               Icons.arrow_circle_right,
-                              color: Colors.yellow,
+                              color: Color.fromARGB(255, 213, 169, 8),
                               size: 40.0,
                             )),
                       ],
@@ -133,6 +137,95 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+        // gradient: LinearGradient(
+        //     stops: [0.2, 0.7],
+        //     begin: Alignment.topCenter,
+        //     end: Alignment.bottomCenter,
+        //     colors: [Color.fromARGB(79, 83, 80, 80), Colors.black]),
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/images/sliderwoman.jpg'),
+        ),
+      ),
+      child: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  stops: [0.1, 0.6],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(168, 51, 50, 50),
+                    Color.fromARGB(223, 0, 0, 0)
+                  ]),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.all(10.0),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.65,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Shop for quality Unisex wears \n right in the comfort of your home',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontFamily: 'Dongle',
+                      height: 0.7,
+                    ),
+                  ),
+                  const SizedBox(height: 40.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.circle,
+                        color: Colors.white,
+                        size: 15.0,
+                      ),
+                      Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                        size: 25.0,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 90.0),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 9.0),
+                      width: MediaQuery.of(context).size.width / 2,
+                      height: MediaQuery.of(context).size.height * 0.06,
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 213, 169, 8),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: const Center(
+                        child: Text(
+                          'Get Started',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30.0,
+                              fontFamily: 'Dongle'),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
